a7a429b0a4591e1618478ee3a15ded10fe36e683
Eddy Garcia <32507458+kirizhu@users.noreply.github.com>
2024-12-12 17:38:51 +0100

    clerk inital wrapping of the app done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,25 @@
+import { tokenCache } from "@/utils/cache";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
+}
+
+const InitialLayout = () => {
   return <Stack />;
+};
+
+export default function RootLayoutNav() {
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 }
